d606310adbb5bc80d4626ce08366130c371dfd74
Rick <rick@imac.myfiosgateway.com>
2022-02-17 21:52:04 -0500

    Added some crap I don't understand. -Matthew Milunic

--- test.py
+++ test.py
@@ -4,10 +4,11 @@ from torch.utils.data import DataLoader
 from torchaudio import datasets, transforms
 import matplotlib.pyplot as plt
 
+# switches to graphics card if I actually had a graphics card. Might be worth an investment
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
-
+# defines the neural network's shape
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
@@ -17,15 +18,30 @@ class NeuralNetwork(nn.Module):
             nn.ReLU(),
             nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(512, 10),
+            nn.Linear(512, 5),
         )
 
     def forward(self, x):
-        pass
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
 
+
+# sets the neuralnetwork to use "device" model
+model = NeuralNetwork().to(device)
+print(model)
+
+softmax = nn.Softmax(dim=1)
 
 training_data = datasets.COMMONVOICE(
     root="/Volumes/External HD/cv-corpus-8.0-2022-01-19/zh-TW", tsv="train.tsv"
 )
 
-print(next(iter(training_data))[2]["sentence"])
+# sets a few paramaters for the training of the model
+learning_rate = 1e-3
+batch_size = 64
+epochs = 5
+
+loss_fn = nn.CrossEntropyLoss()
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
